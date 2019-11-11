d48ea840cf8452bde77e9f91a95f799945f9eec5
Whiskers <33127788+Catz1301@users.noreply.github.com>
2019-11-11 08:49:03 -0500

    Update main.cpp

--- source/src/main.cpp
+++ source/src/main.cpp
@@ -6,15 +6,18 @@ using namespace sf;
 
 sf::Texture stick50;
 Sf::Sprite player;
+const int ground = 400;
 
 int main() {
 	RenderWindow window(VideoMode(800, 600), "The yeeter");
 	
 	stick50.loadFromFile("stickman_50.png");
 	player.setTexture(stick50);
-	int playerX, playerY;
-	playerX = 0;
-	playerY = 400;
+	int playerX = 0, playerY = 400;
+	int speed = 2;
+	
+	bool jump = false;
+	bool goingDown = false;
 	
 	while (window.isOpen()) {
 		// Event processing
@@ -26,22 +29,55 @@ int main() {
 			}
 			else if (event.type == Event::KeyPressd) {
 				if (Keyboard::isKeyPressed(Keyboard::Up)) {
-					playerY -= 1;
+					playerY -= speed;
 				}
 				else if (Keyboard::isKeyPressed(Keyboard::Down)) {
-					playerY += 1;
+					playerY += speed;
 				}
 				else if (Keyboard::isKeyPressed(Keyboard::Left)) {
-					playerX -= 1;
+					playerX -= speed;
 				}
 				else if (Keyboard::isKeyPressed(Keyboard::Right)) {
-					playerX += 1;
+					playerX += speed;
+				}
+				else if (Keyboard::isKeyPressed(Keyboard::Space)) {
+					jump = true;
+				}
+				else if (Keyboard::isKeyPressed(Keyboard::Pause)) {
+					// reset flags...
+					jump = false;
+					goingDown = false;
+					playerX = 0;
+					playerY = 400;
+					player.setPosition(playerX, playerY);
+					window.clear(Color(255,255,0));
+					window.display(); // We will not draw the player.
+					
+			}
+		}
+		
+		if (jump == true) {
+			if (goingDown == false) {
+				if (playerY <= ground - 150) {
+					goingDown = true;
+				}
+				else {
+					playerY -= 1+(0.7*speed);
+				}
+			}
+			else if (goingDown == true) {
+				if (playerY >= ground) {
+					playerY = ground;
+					goingDown = false;
+					jump = false;
+				}
+				else {
+					playerY += speed;
 				}
 			}
 		}
 		
-		
-		player.setPosition(playerX, playerY)
+		player.setPosition(playerX, playerY);
 		window.clear(Color(255,255,255));
 		//window.draw(bg);
 		window.draw(player);
